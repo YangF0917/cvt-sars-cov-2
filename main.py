52488a6a65566f19393a9e4039ebd55234939fcb
Felix <felix.yang0917@gmail.com>
2021-04-29 23:33:43 -0400

    added bar plots for analysis dataset figure 1A

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 hugs_data = pd.read_csv('HUG-ds.csv', sep=',')
-da2f1a = pd.read_csv('dataA2F1A.csv', sep=',')
-
-print(hugs_data)
 
 # HUGS All Age Viz.
 fig, ax = plt.subplots()
+plt.title('Age Distribution of HUGS Data')
 age_d = hugs_data["age"]
 mean = age_d.mean()
 sigma = age_d.std()
@@ -35,4 +33,15 @@ props = dict(boxstyle='round', facecolor='white', alpha=0.5)
 
 ax.text(0.05, 0.95, legstr1, transform=ax.transAxes, fontsize=11,
         verticalalignment='top', bbox=props)
-plt.show()
+
+# SOURCE DATA- Analysis of SARS-CoV-2 antibodies in COVID-19 convalescent blood using a coronavirus antigen microarray
+da2f1a = pd.read_csv('dataA2F1A.csv', sep=',')
+for colNum in range(1,len(da2f1a.columns)):
+    viz1 = da2f1a.iloc[:, [0, colNum]]
+    fig, ax = plt.subplots(figsize=(20, 30))
+    plt.gcf().subplots_adjust(bottom=0.3)
+    ax.bar(viz1.iloc[:, 0], viz1.iloc[:, 1], width=0.5)
+    plt.xticks(rotation='vertical')
+
+# Warning, it generates a lot of graphs, change the indexing above for a particular range
+# plt.show()
